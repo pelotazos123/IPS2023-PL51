33f398a2cc46c1ef4cee9e1f5f09ee2f96acf621
Yago <Yago@Ordenador-Yago>
2023-10-17 19:28:04 +0200

    modificacion datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,8 @@ insert into socios (id,name,surname,cuota_type,iban,height,weight,age,gender,dir
 	(102,'Jose','Alonso','CUOTA_JOVEN','ES6340491500088834566464','1.81',75,24,'HOMBRE',false),
 	(103,'Paula','Perez','CUOTA_JOVEN','ES6000491515051234567692','1.83',78,16,'MUJER',false),
 	(104,'Cristian','Gonzalez','CUOTA_JUBILADO','ES6000491500063234560069','1.78',77,71,'HOMBRE',false),
-	(105,'Pedro','Fernandez','CUOTA_JOVEN','ES6000436501851414507715','1.75',80,23,'HOMBRE',false);
+	(105,'Pedro','Fernandez','CUOTA_JOVEN','ES6000436501851414507715','1.75',80,23,'HOMBRE',false),
+	(106,'Adrian','Garcia','CUOTA_JOVEN','ES6340491500088834566478','1.54',57,14,'HOMBRE',false);
 
 
 
@@ -21,8 +22,15 @@ insert into cuotas (owner_id, cuota_type, price, state) values
 	(103, 'Adulto', 200, 'Emitida'),
 	(103, 'Jubilado', 150, 'Pendiente');
 
-insert into licencias (owner_id, tutor_name, tutor_age, state, price, licence_type, facturation_direction, facturation_info) values 
-	(100, null, null, 'PENDIENTE', 33.33, 'DEPORTISTA', "ESXX-XXXX-XXXX-XXXX-XXXX-XXXX", null);
+insert into licencias (owner_id, tutor_name, tutor_surname, tutor_age, tutor_gender, state, price, licence_type, facturation_direction, facturation_info) values 
+	(100,'noTutor','noTutor',-1,null, 'PENDIENTE', 33.33, 'DEPORTISTA', "ESXX-XXXX-XXXX-XXXX-XXXX-XXXX", null),
+	(104,'noTutor','noTutor',-1,null,'pagado',30,'JUEZ','Vega','Bajo A'),
+	(106,'Pedro','Garcia',40,'HOMBRE','pagado',30,'DEPORTISTA','Oviedo','Segundo A'),
+	(107,'noTutor','noTutor',-1,null,'pagado',30,'DEPORTISTA','Gijon','Segundo A'),
+	(107,'noTutor','noTutor',-1,null,'pagado',30,'JUEZ','Gijon','Segundo A'),
+	(108,'noTutor','noTutor',-1,null,'pagado',30,'DEPORTISTA','Aviles','Segundo A'),
+	(108,'noTutor','noTutor',-1,null,'pagado',30,'JUEZ','Aviles','Tercero B'),
+	(108,'noTutor','noTutor',-1,null,'pagado',30,'MONITOR','Aviles','Tercero B');
 
 
 INSERT INTO reservas (owner_id, fecha, hora, instalation_code) VALUES 
