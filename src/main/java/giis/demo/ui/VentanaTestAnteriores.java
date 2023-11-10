8617fda516ac9d31fa6c8dcae943cec523f864e5
gabigarciam <125141315+gabigarciam@users.noreply.github.com>
2023-11-10 18:03:06 +0100

    Solucionados errores Sprint 2 en ventanas de test Anteriores

--- src/main/java/giis/demo/ui/VentanaTestAnteriores.java
+++ src/main/java/giis/demo/ui/VentanaTestAnteriores.java
@@ -1,6 +1,7 @@
 package giis.demo.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,13 +10,23 @@ import javax.swing.border.EmptyBorder;
 import org.jfree.chart.ChartPanel;
 
 import giis.demo.logica.TestsFisiologicos;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 
-public class VentanaTestAnteriores extends JFrame {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaTestAnteriores extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private VentanaSeleccionTest vst;
 	private TestsFisiologicos tf;
+	private JPanel pnBotones;
+	private JButton btCooper;
+	private JButton btRockport;
+	private JPanel pnGrafica;
 
 	/**
 	 * Create the frame.
@@ -24,18 +35,81 @@ public class VentanaTestAnteriores extends JFrame {
 	 */
 	public VentanaTestAnteriores(VentanaSeleccionTest ventanaSeleccionTest) {
 		this.vst = ventanaSeleccionTest;
-		this.tf = new TestsFisiologicos(vst);
+		this.tf = vst.getTf();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 574, 411);
+		setBounds(100, 100, 676, 483);
+		this.setMinimumSize(new Dimension(676, 483));
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		ChartPanel cp = new ChartPanel(tf.creaGrafica());
 		
+		setModal(true);
 		setContentPane(contentPane);
 		setTitle("Gestión Deportiva");
 		setLocationRelativeTo(null);
-		contentPane.add(cp);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(getPnBotones(), BorderLayout.NORTH);
+		contentPane.add(getPnGrafica());
+		habilitaBotones();
 	}
-
+	private void habilitaBotones() {
+		if(!tf.tieneTestTipo(tf.getId(), TestsFisiologicos.COOPER))
+			getBtCooper().setEnabled(false);
+		if(!tf.tieneTestTipo(tf.getId(), TestsFisiologicos.ROCKPORT))
+			getBtRockport().setEnabled(false);
+	}
+	private JPanel getPnBotones() {
+		if (pnBotones == null) {
+			pnBotones = new JPanel();
+			pnBotones.setLayout(new GridLayout(1, 2, 10, 0));
+			pnBotones.add(getBtCooper());
+			pnBotones.add(getBtRockport());
+		}
+		return pnBotones;
+	}
+	private JButton getBtCooper() {
+		if (btCooper == null) {
+			btCooper = new JButton("COOPER");
+			btCooper.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					muestraGraficaCooper();
+				}
+			});
+			btCooper.setMnemonic('c');
+		}
+		return btCooper;
+	}
+	protected void muestraGraficaCooper() {
+		pnGrafica.removeAll();
+		getPnGrafica().setLayout(new BorderLayout(0, 0));
+		ChartPanel cp = new ChartPanel(tf.creaGrafica(tf.getId(), TestsFisiologicos.COOPER));
+		getPnGrafica().add(cp, BorderLayout.CENTER);
+		getPnGrafica().validate();
+	}
+	private JButton getBtRockport() {
+		if (btRockport == null) {
+			btRockport = new JButton("ROCKPORT");
+			btRockport.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					muestraGraficaRockport();
+				}
+			});
+			btRockport.setMnemonic('r');
+		}
+		return btRockport;
+	}
+	protected void muestraGraficaRockport() {
+		getPnGrafica().removeAll();
+		getPnGrafica().setLayout(new BorderLayout(0, 0));
+		ChartPanel cp = new ChartPanel(tf.creaGrafica(tf.getId(), TestsFisiologicos.ROCKPORT));
+		getPnGrafica().add(cp, BorderLayout.CENTER);
+		getPnGrafica().validate();
+	}
+	
+	private JPanel getPnGrafica() {
+		if (pnGrafica == null) {
+			pnGrafica = new JPanel();
+			pnGrafica.setLayout(new BorderLayout(0, 0));
+		}
+		return pnGrafica;
+	}
 }
